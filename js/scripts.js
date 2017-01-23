6f869e3ae2133a00ea7db5b19bf9fc1f5d5f1208
Jake Campa and Sarah Leahy <jake@>
2017-01-23 14:11:28 -0800

    allow checkbox to be created and removed from the dom

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,17 @@
 $(document).ready(function() {
     $("form.task-list").submit(function(event) {
       event.preventDefault();
+      $(".done").show();
       var inputTask = $("input#task").val();
     $(".output-stream").append('<div class="form-check form-check">'+
             '  <label class="form-check-label">'+
-                '<input class="form-check-input" type="checkbox"'+  'class="checkbox" value="option1"> '+ inputTask +
+                '<input class="form-check-input" type="checkbox"'+  'class="checkbox" name="tasks-given" value="option1"> '+ inputTask +
               '</label>'+
             '</div>')
           });
+     $(".done").click(function() {
+       $("input:checkbox[name=tasks-given]:checked").each(function() {
+         $(this).parent().parent().remove();
+       });
+     });
 });
